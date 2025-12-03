7ed19e210ad2bb69e135d59e9fc8f7864b43319f
yzavorokhina <orangemagicpot@gmail.com>
2025-12-03 21:44:50 +0300

    add game

--- js/index.js
+++ js/index.js
@@ -1,74 +1,103 @@
-import $ from 'jquery';
-import '../scss/custom.scss';
-import _ from 'lodash';
+let currentWord = null;
+let wordLetters = [];
 
-let conn = new XMLHttpRequest();
+//const alphabet = 'АБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ';
+const alphabet = 'абвгдеёжзиклмнопрстуфхцчшщьыъэюя';
+const topics = ["Города", "Транспорт", "Спорт"];
 
-//XPathResult.open(method, URL) - инициализация
-//conn.open('GET', 'https://punkapi.online/v3/beers/random');
-
-//xhr.send([body]) for POST
-// conn.send();
-//xhr.getResponseHeader
-
-//получение ответа:
-//load/error/progress
-
-conn.onerror = function(){
-    console.log(`Не могу соединиться с целевым сервером`);
+const words = {
+    "Города" : ["минск", "москва", "париж", "рим"],
+    "Транспорт" : ["автомобиль", "самолет", "телега", "трамвай"],
+    "Спорт" : ["футбол", "шахматы", "теннис", "бокс"]
 }
 
-conn.onload = function(){
-    if(this.status !== 200) {
-        console.log(`Получен статус ответа ${this.status}. Ошибка!`);
-        return;
+const gameElements = {
+    topic: document.getElementById("game-topic"),
+    word: document.querySelector(".word"),
+    letters: document.querySelector(".letters"),
+    hungman: [
+        document.getElementById("kran"),
+        document.getElementById("head"),
+        document.getElementById("body"),
+        document.getElementById("left-leg"),
+        document.getElementById("right-leg"),
+        document.getElementById("left-arm"),
+        document.getElementById("right-arm")
+    ]   
+}
+
+function init(){
+    let topicIndex = rand(0, topics.length-1);
+    let wordsSet = words[topics[topicIndex]];
+    let wordIndex = rand(0, wordsSet.length-1);
+
+    currentWord = wordsSet[wordIndex];
+
+    gameElements.topic.innerText = topics[topicIndex];
+
+    for (let i = 0; i < currentWord.length; i++){
+        let span = document.createElement("span");
+        span.classList.add("word-letter");
+
+        gameElements.word.append(span);
+
+        let wordLetter = {
+            letter: currentWord[i],
+            element: span
+        }
+
+        wordLetters.push(wordLetter);
     }
-    console.log(this.response);
-    renderBeerCard(this.response);
+
+    for (let i = 0; i < alphabet.length; i++){
+        let button = document.createElement("button");
+        button.classList.add("letter");
+        button.innerText = alphabet[i];
+
+        button.onclick = () => {
+            checkLetter(alphabet[i]);
+        }
+
+        gameElements.letters.append(button);
+    }
 }
 
-// функция смены фона:
-let currentColor;
-const colors = ['#7fff00', '#49d100', '#5ce000', '#6ef000', '#7fff00', '#90ff27', '#a0ff3d', '#6ef000', '#b1ff4f'];
+function checkLetter(letter){
+    //console.log(letter);
+    let pos = 0;
+    let indexes = [];
 
-function changeRandomBackground() {
-  const currentColorIndex = colors.indexOf(currentColor);
-  let nextColor = colors[currentColorIndex + 1] || colors[0];
-  // get random color with lodash:
-  const randomColor1= _.sample(colors);
-  // const randomColor2 = Math.floor(Math.random() * (colors.length - 0 + 1)) + 0;
-  nextColor = randomColor1;
-  // console.log({randomColor2});
+    while(true){
+        let foundPos = currentWord.indexOf(letter, pos);
 
-  document.body.style.background = nextColor;
-  console.log(`new color is ${nextColor}`);
-  currentColor = nextColor;
+        if(foundPos == -1){
+            break;
+        }
+
+        indexes.push(foundPos);
+        pos = foundPos + 1;
+    }
+
+    if(indexes) {
+        // отображаем буквы:
+
+    } else {
+        // отрисовываем часть человека:
+        showHungmanPart();
+    }
+
 }
 
-setInterval( () => {
-    conn.open('GET', 'https://punkapi.online/v3/beers/random');
-    conn.responseType = 'json';
-    conn.send();
-    changeRandomBackground();
-}, 5000);
+function showHungmanPart(){
 
-// TODO: use fetch for response:
-// let response = fetch ('https://punkapi.online/v3/beers/random');
-// console.log(response);
-
-// функция отрисовки карточки с продуктом:
-function renderBeerCard(randomBeer){
-    console.log(randomBeer);
-
-    const productCard = $(".product-card");
-    const { image, name, description } = randomBeer;
-
-    let beer_html = `<div>
-                        <img src="https://punkapi.online/v3/images/${image}" alt="${name}">
-                        <div>
-                            <h5>${name}</h5>
-                            <p>${description}</p>
-                        </div>
-                    </div>`;
-    productCard.html(beer_html); 
+    
 }
+
+function rand(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+init();
